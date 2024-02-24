98d6c09cde4633c91ec9af608e79d9b5b229abb1
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-02-24 15:21:20 -0300

    att camila

--- Sites/script.js
+++ Sites/script.js
@@ -1,14 +1,45 @@
 var type = 0
-function desaparece(){
+function avancar(){
     var foto = document.getElementById("foto")
-    var botao = document.getElementById("botao")
-    if(type == 0){
+    if(type == 4){
         foto.style.backgroundImage = "url(imagens/cortina.jpg)"
-        botao.style.display = "none"
-        type = 1
-    }else{
-        foto.style.backgroundImage = "url(imagens/foto2.jpg)"
-        botao.style.display = "inline-block"
         type = 0
+    }else if(type == 0){
+        foto.style.backgroundImage = "url(imagens/foto1.jpg)"
+        type = 1
     }
+    else if(type == 1){
+        foto.style.backgroundImage = "url(imagens/foto2.jpg)"
+        type = 2
+    }
+    else if(type == 2){
+        foto.style.backgroundImage = "url(imagens/foto3.jpg"
+        type = 3
+    }
+    else{
+        foto.style.backgroundImage = "url(imagens/foto4.jpg)"
+        type = 4
+    }    
+}
+function voltar(){
+    var foto = document.getElementById("foto")
+    if(type == 1){
+        foto.style.backgroundImage = "url(imagens/cortina.jpg)"
+        type = 0
+    }else if(type == 2){
+        foto.style.backgroundImage = "url(imagens/foto1.jpg)"
+        type = 1
+    }
+    else if(type == 3){
+        foto.style.backgroundImage = "url(imagens/foto2.jpg)"
+        type = 2
+    }
+    else if(type == 4){
+        foto.style.backgroundImage = "url(imagens/foto3.jpg)"
+        type = 3
+    }
+    else{
+        foto.style.backgroundImage = "url(imagens/foto4.jpg)"
+        type = 4
+    }    
 }
