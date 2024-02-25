e8bc3776e0e2a4a8b53719e2258b58cf05b5ec7a
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-02-25 14:34:10 -0300

    att

--- Sites/script.js
+++ Sites/script.js
@@ -1,45 +1,25 @@
-var type = 0
+const imagensFundo = ["imagens/cortina.jpg", "imagens/foto1.jpg", "imagens/foto2.jpg", "imagens/foto3.jpg", "imagens/foto4.jpg", "imagens/foto5.jpg", "imagens/foto6.jpg"];
+let indiceAtual = 0;
+const foto = document.getElementById("foto")
+
 function avancar(){
-    var foto = document.getElementById("foto")
-    if(type == 4){
-        foto.style.backgroundImage = "url(imagens/cortina.jpg)"
-        type = 0
-    }else if(type == 0){
-        foto.style.backgroundImage = "url(imagens/foto1.jpg)"
-        type = 1
-    }
-    else if(type == 1){
-        foto.style.backgroundImage = "url(imagens/foto2.jpg)"
-        type = 2
-    }
-    else if(type == 2){
-        foto.style.backgroundImage = "url(imagens/foto3.jpg"
-        type = 3
-    }
-    else{
-        foto.style.backgroundImage = "url(imagens/foto4.jpg)"
-        type = 4
-    }    
+    indiceAtual = (indiceAtual + 1) % imagensFundo.length;
+    const proximaImagemURL = imagensFundo[indiceAtual];
+    foto.style.opacity = 0;
+    setTimeout(function() {
+        foto.style.backgroundImage = `url('${proximaImagemURL}')`,
+        foto.style.opacity = 1;
+    }, 300);
 }
 function voltar(){
-    var foto = document.getElementById("foto")
-    if(type == 1){
-        foto.style.backgroundImage = "url(imagens/cortina.jpg)"
-        type = 0
-    }else if(type == 2){
-        foto.style.backgroundImage = "url(imagens/foto1.jpg)"
-        type = 1
+    if(indiceAtual == 0){        
+        indiceAtual = imagensFundo.length;
     }
-    else if(type == 3){
-        foto.style.backgroundImage = "url(imagens/foto2.jpg)"
-        type = 2
-    }
-    else if(type == 4){
-        foto.style.backgroundImage = "url(imagens/foto3.jpg)"
-        type = 3
-    }
-    else{
-        foto.style.backgroundImage = "url(imagens/foto4.jpg)"
-        type = 4
-    }    
+    indiceAtual = (indiceAtual - 1) % imagensFundo.length;
+    const proximaImagemURL = imagensFundo[indiceAtual];
+    foto.style.opacity = 0;
+    setTimeout(function() {
+        foto.style.backgroundImage = `url('${proximaImagemURL}')`,
+        foto.style.opacity = 1;
+    }, 300);  
 }
