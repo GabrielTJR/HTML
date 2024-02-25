57a8734f55adb97fa746240a540076bf1c5d5d1d
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-02-25 17:35:25 -0300

    att

--- Sites/script.js
+++ Sites/script.js
@@ -1,6 +1,9 @@
-const imagensFundo = ["imagens/cortina.jpg", "imagens/foto1.jpg", "imagens/foto2.jpg", "imagens/foto3.jpg", "imagens/foto4.jpg", "imagens/foto5.jpg", "imagens/foto6.jpg"];
+const imagensFundo = ["imagens/foto1.jpg", "imagens/foto2.jpg", "imagens/foto3.jpg", "imagens/foto4.jpg", "imagens/foto5.jpg", "imagens/foto6.jpg"];
 let indiceAtual = 0;
 const foto = document.getElementById("foto")
+const fototext1 = document.getElementById("foto1")
+const fototext2 = document.getElementById("foto2")
+
 
 function avancar(){
     indiceAtual = (indiceAtual + 1) % imagensFundo.length;
@@ -23,3 +26,13 @@ function voltar(){
         foto.style.opacity = 1;
     }, 300);  
 }
+
+setInterval(function() {
+    indiceAtual = (indiceAtual + 1) % imagensFundo.length;
+    const proximaImagemURL = imagensFundo[indiceAtual];
+    fototext1.style.opacity = 0;
+    setTimeout(function() {
+        fototext1.style.backgroundImage = `url('${proximaImagemURL}')`,
+        fototext1.style.opacity = 1;
+    }, 300);
+}, 5000);
