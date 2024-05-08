b35a58bc88a1bd6bf99fdfd8223ee69e50f3d779
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-05-08 12:49:10 -0300

    ATT

--- Trabalho_grafos/script.js
+++ Trabalho_grafos/script.js
@@ -1,67 +1,28 @@
 function generateSudoku() {
-    const order = parseInt(document.getElementById('order').value);
-    const startRow = parseInt(document.getElementById('startRow').value) - 1;
-    const startCol = parseInt(document.getElementById('startCol').value) - 1;
-    const Raiz = Math.sqrt(order);
-    if(Number.isInteger(Raiz)){    
-        const sudoku = generateSudokuGrid(order, startRow, startCol);
-        drawSudoku(sudoku);
-        createSudokuGraph(sudoku);
+    const ordem = parseInt(document.getElementById('ordem').value);
+    const LinhaIni = parseInt(document.getElementById('LinhaIni').value) - 1;
+    const ColunaIni = parseInt(document.getElementById('ColunaIni').value) - 1;
+    const Raiz = Math.sqrt(ordem);
+    if(Number.isInteger(Raiz)){
+        createSudokuGraph(ordem);
     }else{
         alert('Digite um número com raiz inteira para a ordem da matriz');
     }
 }
 
-function generateSudokuGrid(order, startRow, startCol) {
-    const grid = [];
-    const subgridSize = Math.sqrt(order);
-    let num = 1;
-
-    for (let i = 0; i < order; i++) {
-        const row = [];
-        for (let j = 0; j < order; j++) {
-            const rowIndex = (startRow + i) % order;
-            const colIndex = (startCol + j) % order;
-            const value = (num + rowIndex + colIndex * subgridSize - 1) % order + 1;
-            row.push(value);
-            num = value;
-        }
-        num = (num + subgridSize) % order + 1;
-        grid.push(row);
-    }
-
-    return grid;
-}
-
-function drawSudoku(grid) {
-    const table = document.getElementById('sudoku');
-    table.innerHTML = '';
-
-    for (let i = 0; i < grid.length; i++) {
-        const row = document.createElement('tr');
-        for (let j = 0; j < grid[i].length; j++) {
-            const cell = document.createElement('td');
-            cell.textContent = grid[i][j];
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
-    }
-}
-
-function createSudokuGraph(sudoku) {
-    var tamanho = sudoku.length;
-    var nodes = [];
-    var edges = [];
+function createSudokuGraph(ordem) {
+    var tamanho = ordem;
+    var nos = [];
+    var arestas = [];
 
     // Criar nós
     for (var i = 0; i < tamanho; i++) {
         for (var j = 0; j < tamanho; j++) {
             var id = i * tamanho + j;
-            var label = sudoku[i][j] !== 0 ? sudoku[i][j].toString() : '';
+            var label = '';
             var x = (j + 1) * 100;
             var y = (i + 1) * 100;
-            var group = sudoku[i][j]; // Define o grupo de cada nó como o valor do sudoku
-            nodes.push({ id: id, label: label, x: x, y: y, group: group });
+            nos.push({ id: id, label: label, x: x, y: y});
         }
     }
 
@@ -73,62 +34,54 @@ function createSudokuGraph(sudoku) {
             // Arestas na mesma linha
             for (var k = 0; k < tamanho; k++) {
                 if (k !== j) {
-                    edges.push({ from: id, to: i * tamanho + k });
+                    arestas.push({ from: id, to: i * tamanho + k });
                 }
             }
 
             // Arestas na mesma coluna
             for (var k = 0; k < tamanho; k++) {
                 if (k !== i) {
-                    edges.push({ from: id, to: k * tamanho + j });
+                    arestas.push({ from: id, to: k * tamanho + j });
                 }
             }
 
             // Arestas no mesmo bloco
             var blockSize = Math.sqrt(tamanho);
-            var blockStartRow = Math.floor(i / blockSize) * blockSize;
-            var blockStartCol = Math.floor(j / blockSize) * blockSize;
-            for (var k = blockStartRow; k < blockStartRow + blockSize; k++) {
-                for (var l = blockStartCol; l < blockStartCol + blockSize; l++) {
+            var blockLinhaIni = Math.floor(i / blockSize) * blockSize;
+            var blockColunaIni = Math.floor(j / blockSize) * blockSize;
+            for (var k = blockLinhaIni; k < blockLinhaIni + blockSize; k++) {
+                for (var l = blockColunaIni; l < blockColunaIni + blockSize; l++) {
                     if (k !== i || l !== j) {
-                        edges.push({ from: id, to: k * tamanho + l });
+                        arestas.push({ from: id, to: k * tamanho + l });
                     }
                 }
             }
         }
     }
-
-    // Ordenar os vértices em ordem não crescente de grau
-    nodes.sort((a, b) => {
-        const degreeA = edges.filter(edge => edge.from === a.id || edge.to === a.id).length;
-        const degreeB = edges.filter(edge => edge.from === b.id || edge.to === b.id).length;
-        return degreeB - degreeA;
-    });
-
+    
     // Colorir os vértices
-    var colors = {};
-    var colorIndex = 1;
-    var colored = {};
-    nodes.forEach(function(node) {
+    var colorido = {};
+    nos.forEach(function(node) {
         var id = node.id;
-        var adjacentColored = {};
+        var adjacentcolorido = {};
 
         // Verificar cores dos vértices adjacentes
-        edges.forEach(function(edge) {
-            if (edge.from === id && colored[edge.to]) {
-                adjacentColored[colored[edge.to]] = true;
+        arestas.forEach(function(edge) {
+            if (edge.from === id && colorido[edge.to]) {
+                adjacentcolorido[colorido[edge.to]] = true;
             }
-            if (edge.to === id && colored[edge.from]) {
-                adjacentColored[colored[edge.from]] = true;
+            if (edge.to === id && colorido[edge.from]) {
+                adjacentcolorido[colorido[edge.from]] = true;
             }
         });
 
         // Encontrar a primeira cor disponível para o vértice
         var colorFound = false;
         for (var i = 1; !colorFound; i++) {
-            if (!adjacentColored[i]) {
+            if (!adjacentcolorido[i]) {
                 node.color = { background: "hsl(" + (360 * i / tamanho) + ",100%,70%)" };
-                colored[id] = i;
+                node.label = i.toString();
+                colorido[id] = i;
                 colorFound = true;
             }
         }
@@ -137,14 +90,14 @@ function createSudokuGraph(sudoku) {
     // Opções de configuração do gráfico
     var options = {
         physics: false, // Desabilitar a simulação física para evitar que os vértices se movam
-        nodes: {
+        nos: {
             shape: "circle",
             tamanho: 20
         }
     };
 
     // Criação do gráfico
-    var container = document.getElementById('graph');
-    var data = { nodes: nodes, edges: edges };
+    var container = document.getElementById('grafo');
+    var data = { nodes: nos, edges: arestas };
     var network = new vis.Network(container, data, options);
 }
