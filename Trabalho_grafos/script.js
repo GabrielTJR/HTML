840562ecadcb03fdb07f9bcbf229e7b0f108eef5
Gabriel Toscano Jorge Rodrigues <gabriel.rodrigues@unimedlitoral.coop.br>
2024-05-08 17:56:25 -0300

    att

--- Trabalho_grafos/script.js
+++ Trabalho_grafos/script.js
@@ -4,25 +4,64 @@ function generateSudoku() {
     const ColunaIni = parseInt(document.getElementById('ColunaIni').value) - 1;
     const Raiz = Math.sqrt(ordem);
     if(Number.isInteger(Raiz)){
-        createSudokuGraph(ordem);
+        createSudokuGraph(ordem, LinhaIni, ColunaIni);
+        
     }else{
         alert('Digite um número com raiz inteira para a ordem da matriz');
     }
 }
 
-function createSudokuGraph(ordem) {
-    var tamanho = ordem;
+function generateSudokuGrid(order) {
+    const grid = [];
+    const subgridSize = Math.sqrt(order);
+    let num = 1;
+
+    // Gera um sudoku com distribuição começando da primeira linha e primeira coluna
+    for (let i = 0; i < order; i++) {
+        const row = [];
+        for (let j = 0; j < order; j++) {
+            const value = (i * subgridSize + Math.floor(i / subgridSize) + j) % order + 1;
+            row.push(value);
+            num = value;
+        }
+        num = (num + subgridSize) % order + 1;
+        grid.push(row);
+    }
+
+    return grid;
+}
+
+function createSudokuGraph(sudoku, LinhaIni, ColunaIni) {
+    var tamanho = sudoku.length;
     var nos = [];
     var arestas = [];
+    const subgridSize = Math.sqrt(ordem);
 
     // Criar nós
     for (var i = 0; i < tamanho; i++) {
         for (var j = 0; j < tamanho; j++) {
             var id = i * tamanho + j;
-            var label = '';
+            var label = sudoku[i][j] !== 0 ? sudoku[i][j].toString() : '';
             var x = (j + 1) * 100;
             var y = (i + 1) * 100;
             nos.push({ id: id, label: label, x: x, y: y});
+        }
+    }
+
+    function findNodeById(nodeId) {
+        return nos.find(function(node) {
+            return node.id === nodeId;
+        });
+    }
+
+    for (var i = 0; i < tamanho; i++) {
+        for (var j = 0; j < tamanho; j++) {
+            var id = ((LinhaIni+i)%tamanho) * tamanho + ((ColunaIni+j)%tamanho);
+            const value = (i * subgridSize + Math.floor(i / subgridSize) + j) % ordem + 1;
+            var node = findNodeById(id);
+            if (node) {
+                node.label = value.toString();
+            }
         }
     }
 
@@ -33,14 +72,14 @@ function createSudokuGraph(ordem) {
 
             // Arestas na mesma linha
             for (var k = 0; k < tamanho; k++) {
-                if (k !== j) {
+                if (k != j) {
                     arestas.push({ from: id, to: i * tamanho + k });
                 }
             }
 
             // Arestas na mesma coluna
             for (var k = 0; k < tamanho; k++) {
-                if (k !== i) {
+                if (k != i) {
                     arestas.push({ from: id, to: k * tamanho + j });
                 }
             }
@@ -51,7 +90,7 @@ function createSudokuGraph(ordem) {
             var blockColunaIni = Math.floor(j / blockSize) * blockSize;
             for (var k = blockLinhaIni; k < blockLinhaIni + blockSize; k++) {
                 for (var l = blockColunaIni; l < blockColunaIni + blockSize; l++) {
-                    if (k !== i || l !== j) {
+                    if (k != i || l != j) {
                         arestas.push({ from: id, to: k * tamanho + l });
                     }
                 }
@@ -60,17 +99,17 @@ function createSudokuGraph(ordem) {
     }
     
     // Colorir os vértices
-    var colorido = {};
+    /*var colorido = {};
     nos.forEach(function(node) {
         var id = node.id;
         var adjacentcolorido = {};
 
         // Verificar cores dos vértices adjacentes
         arestas.forEach(function(edge) {
-            if (edge.from === id && colorido[edge.to]) {
+            if (edge.from == id && colorido[edge.to]) {
                 adjacentcolorido[colorido[edge.to]] = true;
             }
-            if (edge.to === id && colorido[edge.from]) {
+            if (edge.to == id && colorido[edge.from]) {
                 adjacentcolorido[colorido[edge.from]] = true;
             }
         });
@@ -85,18 +124,23 @@ function createSudokuGraph(ordem) {
                 colorFound = true;
             }
         }
+    });*/
+
+    // Colorir o grafo
+    nos.forEach(function(node) {
+        node.color = { background: "hsl(" + (360 * node.label / tamanho) + ",100%,70%)" };
     });
 
-    // Opções de configuração do gráfico
+    // Opções de configuração do grafo
     var options = {
-        physics: false, // Desabilitar a simulação física para evitar que os vértices se movam
+        physics: false,
         nos: {
             shape: "circle",
             tamanho: 20
         }
     };
 
-    // Criação do gráfico
+    // Criação do grafo
     var container = document.getElementById('grafo');
     var data = { nodes: nos, edges: arestas };
     var network = new vis.Network(container, data, options);
