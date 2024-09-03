f58aa07a6328eede00c200621c7b0e02ef8018f0
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-09-03 00:07:58 -0300

    att

---
+++ ProgramaçãoWeb/Trabalho1/script.js
@@ -0,0 +1,15 @@
+function selecionarFilme(filme){
+    if(filme == 1){
+
+    }else if(filme == 2){
+
+    }else if(filme == 3){
+
+    }else if(filme == 4){
+
+    }else if(filme == 5){
+
+    }else if(filme == 6){
+
+    }
+}
