5b2700d231f197c57012bb28f9caff3f84021922
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-09-03 12:45:00 -0300

    ATT

--- ProgramaçãoWeb/Trabalho1/script.js
+++ ProgramaçãoWeb/Trabalho1/script.js
@@ -1,6 +1,23 @@
+var card1 = document.getElementById("filme1");
+var card2 = document.getElementById("filme2");
+var card3 = document.getElementById("filme3");
+var card4 = document.getElementById("filme4");
+var card5 = document.getElementById("filme5");
+var card6 = document.getElementById("filme6");
+
+
+var describe1 = document.getElementById("describe1");
+var describe2 = document.getElementById("describe2");
+var describe3 = document.getElementById("describe3");
+var describe4 = document.getElementById("describe4");
+var describe5 = document.getElementById("describe5");
+var describe6 = document.getElementById("describe6");
+
 function selecionarFilme(filme){
     if(filme == 1){
-
+        
+        card1.style.display = "none";
+        describe1.style.display = "block";
     }else if(filme == 2){
 
     }else if(filme == 3){
