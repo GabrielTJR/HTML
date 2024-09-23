2d2cafd3dca485152ca275d40b6f5a2ca039956f
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-09-23 17:15:27 -0300

    att

--- ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
+++ ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
@@ -1,33 +1,36 @@
-function getPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon/?offset=200&limit=50')
-    .then(response => response.json())
-    .then(pokemons => {
+function getPokemons() {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=500')
+        .then(response => response.json())
+        .then(pokemons => {
+            const cards = document.getElementById('posts');
+            const consultas = pokemons.results.map(pokemon => 
+                fetch(pokemon.url)
+                    .then(response => response.json())
+                    .then(pokemon_1url => 
+                        fetch(pokemon_1url.forms[0].url)
+                            .then(response => response.json())
+                            .then(pokemon_2url => ({
+                                name: pokemon.name,
+                                img: pokemon_2url.sprites.front_default,
+                                id: pokemon_2url.id
+                            }))
+                    )
+            );
 
-        const cards = document.getElementById('posts');
-        
-        var img, id;
-        pokemons.results.forEach(pokemon => {
-            const card = document.createElement('div');
-            fetch(pokemon.url)
-            .then(response => response.json())
-            .then(pokemon_1url => {
-                fetch(pokemon_1url.forms[0].url)
-                .then(response => response.json())
-                .then(pokemon_2url => {
-                    img = pokemon_2url.sprites.front_default;
-                    id = pokemon_2url.id;
-                    card.className = 'card col-2 m-2'
-                    card.innerHTML = `<img src="${img}">
-                    <div class='card-body pb-0'>
-                        <h2 class = 'card-title fs-6 pb-0'>${pokemon.name} </h2>
-                    </div>
-                    <div class = 'card-footer text-end'>ID: ${id} </div>`;
-
+            Promise.all(consultas).then(pokemons => {
+                pokemons.forEach(({ name, img, id }) => {
+                    const card = document.createElement('div');
+                    card.className = 'card col-2 m-2';
+                    card.innerHTML = `
+                        <img src="${img}">
+                        <div class='card-body pb-0'>
+                            <h2 class='card-title fs-6 pb-0'>${name}</h2>
+                        </div>
+                        <div class='card-footer text-end'>ID: ${id}</div>`;
                     cards.appendChild(card);
-                })
-            })
+                });
+            });
         });
-    })
 }
 
 document.addEventListener('DOMContentLoaded', getPokemons)
