18e45b12a89459f36597e8ebb3b3ed71ab8f58c9
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-09-23 15:34:56 -0300

    att

--- ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
+++ ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
@@ -1,12 +1,11 @@
-var img, id;
 function getPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon/?offset=50&limit=50')
+    fetch('https://pokeapi.co/api/v2/pokemon/?offset=200&limit=50')
     .then(response => response.json())
     .then(pokemons => {
 
         const cards = document.getElementById('posts');
         
-
+        var img, id;
         pokemons.results.forEach(pokemon => {
             const card = document.createElement('div');
             fetch(pokemon.url)
@@ -17,18 +16,16 @@ function getPokemons(){
                 .then(pokemon_2url => {
                     img = pokemon_2url.sprites.front_default;
                     id = pokemon_2url.id;
+                    card.className = 'card col-2 m-2'
+                    card.innerHTML = `<img src="${img}">
+                    <div class='card-body pb-0'>
+                        <h2 class = 'card-title fs-6 pb-0'>${pokemon.name} </h2>
+                    </div>
+                    <div class = 'card-footer text-end'>ID: ${id} </div>`;
+
+                    cards.appendChild(card);
                 })
             })
-            
-
-            card.className = 'card col-2 m-2'
-            card.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${img}.png">
-            <div class='card-body pb-0'>
-                <h2 class = 'card-title fs-6 pb-0'>${pokemon.name} </h2>
-            </div>
-            <div class = 'card-footer text-end'>ID: ${id} </div>`;
-
-            cards.appendChild(card);
         });
     })
 }
