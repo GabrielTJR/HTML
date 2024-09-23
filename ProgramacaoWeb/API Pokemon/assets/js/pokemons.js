4ec583472459724f484aae2f0b921637e7fe7f6f
Gabriel Toscano Jorge Rodrigues <gabriel.rodrigues@unimedlitoral.coop.br>
2024-09-23 15:14:57 -0300

    att

--- ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
+++ ProgramacaoWeb/API Pokemon/assets/js/pokemons.js
@@ -1,18 +1,32 @@
+var img, id;
 function getPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
+    fetch('https://pokeapi.co/api/v2/pokemon/?offset=50&limit=50')
     .then(response => response.json())
     .then(pokemons => {
 
         const cards = document.getElementById('posts');
+        
 
-        pokemons.results.forEach((pokemon, index) => {
+        pokemons.results.forEach(pokemon => {
             const card = document.createElement('div');
+            fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokemon_1url => {
+                fetch(pokemon_1url.forms[0].url)
+                .then(response => response.json())
+                .then(pokemon_2url => {
+                    img = pokemon_2url.sprites.front_default;
+                    id = pokemon_2url.id;
+                })
+            })
+            
+
             card.className = 'card col-2 m-2'
-            card.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png">
+            card.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${img}.png">
             <div class='card-body pb-0'>
                 <h2 class = 'card-title fs-6 pb-0'>${pokemon.name} </h2>
             </div>
-            <div class = 'card-footer text-end'>ID: ${index+1} </div>`;
+            <div class = 'card-footer text-end'>ID: ${id} </div>`;
 
             cards.appendChild(card);
         });
