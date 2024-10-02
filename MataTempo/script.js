f582a800a25fb92e09c1400101e7d4646e24734b
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-10-02 11:28:32 -0300

    att

--- MataTempo/script.js
+++ MataTempo/script.js
@@ -1,5 +1,7 @@
 const button1 = document.getElementById('button1')
 const button2 = document.getElementById('button2')
+const pergunta = document.getElementById('pergunta')
+let confirmadas = 0
 
 function posicionarBotao() {
     const container = document.getElementById('main');
@@ -19,4 +21,28 @@ function posicionarBotao() {
     // Aplicando a nova posição ao botão
     button2.style.left = `${posX}px`;
     button2.style.top = `${posY}px`;
+}
+
+function confirmar(){
+    switch(confirmadas){
+        case 0:
+            pergunta.innerText = "Tem certeza?"
+            break
+        case 1:
+            pergunta.innerText = "Absoluta?"
+            break
+        case 2:
+            pergunta.innerText = "Não é mentira?"
+            break
+        case 3:
+            pergunta.innerText = "Bastante?"
+            break
+        case 4:
+            pergunta.innerText = "Do fundo do coração?"
+            break
+        case 5:
+            pergunta.innerText = ""
+            break
+    }
+    confirmadas++
 }
