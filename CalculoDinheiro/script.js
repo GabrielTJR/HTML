c43ed780db75b4e4bd4a6f616a0f6f6e17123975
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-10-04 15:01:02 -0300

    att

--- CalculoDinheiro/script.js
+++ CalculoDinheiro/script.js
@@ -1,23 +1,49 @@
-var gastos=[], ganhos=0, sobra;
+var gastos=[], ganhos=0, sobra, poupanca=0, ganho;
+const valorPoupanca = document.getElementById('valorPoupanca')
 const valorGanhos = document.getElementById('valorGanhos')
 const valorGastos = document.getElementById('valorGastos')
+
+function adicionarPoupanca(){
+    if(document.getElementById('poupanca').value == ''){
+        poupanca=0
+    }else{
+        poupanca += parseFloat(document.getElementById('poupanca').value)
+    }
+    valorPoupanca.innerText = `Valor em poupança: R$${poupanca}`
+}
+
+function adicionarGanhos(){
+    if(document.getElementById('ganho').value == ''){
+        ganho=0
+    }else{
+        ganho = parseFloat(document.getElementById('ganho').value)
+    }
+    ganhos+= ganho;
+    valorGanhos.innerText = `Valor ganho por mês: R$${ganhos}`
+}
 
 function adicionarGastos(){
     gasto = parseFloat(document.getElementById('gasto').value)
     var nomeGasto = document.getElementById('nomeGasto').value
     var parcelas = parseInt(document.getElementById('parcelas').value)
-    gastos += {nome:nomeGasto, valor:gasto, parcelas:parcelas}
-    valorGastos.innerHTML += `Pagamento de ${nomeGasto}: R$${gasto}, durante ${parcelas} parcelas.<br>`
-}
-
-function adicionarGanhos(){
-    ganho = parseFloat(document.getElementById('ganho').value)
-    ganhos+= ganho;
-    valorGanhos.innerText = `Valor ganho por mês: R$${ganhos}`
+    if(nomeGasto == '' || document.getElementById('parcelas').value == '' || document.getElementById('gasto').value == ''){
+        alert("Preencha todas as informações")
+    }else{
+        gastos += {nome:nomeGasto, valor:gasto, parcelas:parcelas}
+        valorGastos.innerHTML += `Pagamento de ${nomeGasto}: R$${gasto}, ${parcelas}x.<br>`
+        gastos.forEach((nome, valor, parcelas)=>{
+            
+        });
+    }
 }
 
 function gerarSobra(){
 
+}
+
+function limparPoupanca(){
+    poupanca=0;
+    valorPoupanca.innerText =''
 }
 
 function limparGanhos(){
