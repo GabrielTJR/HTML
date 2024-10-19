04dbaf3ccd46892c3a0b648d6480fa19e5f52c06
GabrielTJR <108480674+GabrielTJR@users.noreply.github.com>
2024-10-18 21:09:41 -0300

    att prova2

---
+++ Prova2/script.js
@@ -0,0 +1,34 @@
+function loadNavBar(){
+
+    fetch('components/navbar.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('header').innerHTML = data)
+}
+
+document.addEventListener('DOMContentLoaded', loadNavBar)
+
+function loadSelect(){
+    var select = document.getElementById('select');
+    for(i=0; i<7; i++){
+        select.innerHTML += 
+        `<select class="p-3 col form-select" aria-label="Default select example">
+            <option selected>Open this select menu</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+        </select>`
+        
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadSelect)
+
+function loadMiniCard(){
+    for(i=0; i<12; i++){
+        fetch('components/minicard.html')
+            .then(response => response.text())
+            .then(data => document.getElementById('cards').innerHTML += data)
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadMiniCard)
